c809d5369570352888d6c2d937543afcd3d6639e
MayeLabs <mayeaguilera96@gmail.com>
2025-04-05 07:16:40 -0500

    Agregar boton y ajustar el scroll vertical

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
     const rocket = document.getElementById("rocket");
+    const startButton = document.getElementById("start-button");
+    const startScreen = document.getElementById("start-screen");
+
     let x = 50;
     let y = 50;
     const speed = 10;
+    let gameStarted = false;
+    
+    startButton.addEventListener("click", function(){
+        startScreen.style.display = "none";
+        gameStarted = true;
+
+    })
 
     document.addEventListener("keydown", function(event) {
+        if (!gameStarted) return;
+        else {
+            if ( event.key === "ArrowUp" || event.key === "ArrowDown" )
+            {
+                    event.preventDefault();
+            }
+        }
         switch (event.key) {
             case "ArrowUp":
                 y -= speed;
